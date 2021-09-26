bc4ca95203a7e7553cb16ed941db9de588416096
kolka72 <kolia199972@gmail.com>
2021-09-26 21:38:19 +0300

    modified code

--- 04_board/app.js
+++ 04_board/app.js
@@ -6,24 +6,25 @@ for (let i = 0; i < SQUARE_NUMBERS; i++) {
   const square = document.createElement('div')
   square.classList.add('square')
 
-  square.addEventListener('mouseover', () => setColor(square))
-  square.addEventListener('mouseleave', () => removeColor(square))
+  square.addEventListener('mouseover', setColor)
+  square.addEventListener('mouseleave', removeColor)
 
   board.append(square)
 }
 
-function setColor(element) {
+function setColor(event) {
+  const element = event.target
   const color = getColor()
   element.style.background = color
   element.style.boxShadow = `0 0 5px ${color}, 0 0 50px ${color}`
 } 
 
-function removeColor(element) {
+function removeColor(event) {
+  const element = event.target
   element.style.background = '#1d1d1d'
   element.style.boxShadow = `0 0 2px #000`
 }
 
 function getColor() {
-  const index = Math.floor(Math.random() * colors.length)
-  return colors[index]
+  return colors[Math.floor(Math.random() * colors.length)]
 }
